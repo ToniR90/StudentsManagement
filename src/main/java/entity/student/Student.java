670f894c71e62c1aec364f0769d17cc975015382
ToniR90 <romy90.t@gmail.com>
2025-08-12 12:43:01 +0200

    feat: Student class

--- src/main/java/entity/student/Student.java
+++ src/main/java/entity/student/Student.java
@@ -3,6 +3,7 @@ package entity.student;
 import entity.Participant;
 import entity.Session;
 import entity.student.studentEnum.*;
+import jakarta.persistence.Column;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
@@ -10,13 +11,31 @@ import java.util.List;
 
 @SuperBuilder
 public class Student extends Participant {
+
+    @Column(name = "degree" , nullable = false)
     private Degree degree;
+
+    @Column(name = "study_year")
     private StudyYear studyYear;
+
+    @Column(name = "is_alumni" , nullable = false)
     private boolean isAlumni;
+
+    @Column(name = "alumni_type")
     private AlumniType alumniType;
+
+    @Column(name = "service_awareness" , nullable = false)
     private ServiceAwareness serviceAwareness;
+
+    @Column(name = "first_contact_date" , nullable = false)
     private LocalDate firstContactDate;
+
+    @Column(name = "personal_observations")
     private String personalObservation;
+
+    @Column(name = "rgpd_status")
     private RGPD_Status rgpd_status;
+
+
     private List<Session> sessionList;
 }
