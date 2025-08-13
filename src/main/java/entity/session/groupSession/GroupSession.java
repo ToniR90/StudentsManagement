808afd6e709195acf8202ae37cbbc46ad6605478
ToniR90 <romy90.t@gmail.com>
2025-08-13 14:53:55 +0200

    feat: group session entity

--- src/main/java/entity/session/groupSession/GroupSession.java
+++ src/main/java/entity/session/groupSession/GroupSession.java
@@ -2,8 +2,10 @@ package entity.session.groupSession;
 
 import entity.participant.abstractParticipant.Participant;
 import entity.session.abstractSession.Session;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +22,36 @@ import java.util.Set;
 @AllArgsConstructor
 public class GroupSession extends Session {
 
+    @ManyToMany
+    @JoinTable(
+            name = "group_session_assistants",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "participant_id")
+    )
     private Set<Participant> assistants;
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_session_collaborators",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "participant_id")
+    )
     private Set<Participant> collaborators;
+
+    @DecimalMin(value = "0.0" , message = "Assistance must be non-negative")
+    @Column(name = "assistance")
     private double assistance;
+
+    @Size(max = 100)
+    @NotBlank
+    @Column(name = "classroom")
     private String classroom;
+
+    @ElementCollection
+    @CollectionTable(name = "group_session_material" , joinColumns = @JoinColumn(name = "session_id"))
     private List<String> material;
+
+    @Size(max = 100)
+    @Column(name = "class_ticket")
     private String classTicket;
 }
