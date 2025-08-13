1034e2aef7ec92eddd5e949eca869ff43c20a834
ToniR90 <romy90.t@gmail.com>
2025-08-13 13:34:32 +0200

    feat: group session first deployment

--- src/main/java/entity/session/groupSession/GroupSession.java
+++ src/main/java/entity/session/groupSession/GroupSession.java
@@ -1,4 +1,29 @@
 package entity.session.groupSession;
 
-public class GroupSession {
+import entity.participant.abstractParticipant.Participant;
+import entity.session.abstractSession.Session;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "group_session")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupSession extends Session {
+
+    private Set<Participant> assistants;
+    private Set<Participant> collaborators;
+    private double assistance;
+    private String classroom;
+    private List<String> material;
+    private String classTicket;
 }
