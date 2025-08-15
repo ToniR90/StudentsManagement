0bae0f21648880bf77b64edee744cc02ce33adb3
ToniR90 <romy90.t@gmail.com>
2025-08-15 23:37:54 +0200

    feat: StudentResponseDTO

--- src/main/java/dto/student/StudentResponseDTO.java
+++ src/main/java/dto/student/StudentResponseDTO.java
@@ -1,4 +1,32 @@
 package dto.student;
 
+import entity.participant.abstractParticipant.participantEnum.RGPD_Status;
+import entity.participant.student.studentEnum.AlumniType;
+import entity.participant.student.studentEnum.Degree;
+import entity.participant.student.studentEnum.ServiceAwareness;
+import entity.participant.student.studentEnum.StudyYear;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
 public class StudentResponseDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String secondSurname;
+    private String email;
+    private RGPD_Status rgpdStatus;
+    private Set<String> extraEmails;
+    private Degree degree;
+    private StudyYear studyYear;
+    private Boolean isAlumni;
+    private AlumniType alumniType;
+    private ServiceAwareness serviceAwareness;
+    private String serviceAwarenessDisplayLabel;
+    private LocalDate firstContactDate;
+    private String personalObservation;
 }
