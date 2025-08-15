e9da3cf0195f62efdd794a8d3f75e6123f5d0df4
ToniR90 <romy90.t@gmail.com>
2025-08-15 23:50:09 +0200

    feat: StudentResponseDTO implementation

--- src/main/java/dto/student/StudentResponseDTO.java
+++ src/main/java/dto/student/StudentResponseDTO.java
@@ -5,6 +5,7 @@ import entity.participant.student.studentEnum.AlumniType;
 import entity.participant.student.studentEnum.Degree;
 import entity.participant.student.studentEnum.ServiceAwareness;
 import entity.participant.student.studentEnum.StudyYear;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@Builder
 public class StudentResponseDTO {
     private Long id;
     private String name;
