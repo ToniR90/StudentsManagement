369d1aef47ce70d2a00e6c4a44a1bb3d8636ff50
ToniR90 <romy90.t@gmail.com>
2025-08-21 03:32:24 +0200

    feat: GroupSessionRepository

--- src/main/java/repository/GroupSessionRepository.java
+++ src/main/java/repository/GroupSessionRepository.java
@@ -3,5 +3,10 @@ package repository;
 import entity.session.groupSession.GroupSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupSessionRepository extends JpaRepository<GroupSession , Long> {
+
+    List<GroupSession> findByClassroomIgnoreCase(String classroom);
+    List<GroupSession> findByClassTicketIgnoreCase(String classTicket);
 }
