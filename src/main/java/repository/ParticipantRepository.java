14c2789e5d3507511217fb3198f8cb81e2d1be5f
ToniR90 <romy90.t@gmail.com>
2025-08-21 02:56:41 +0200

    Refactor: Participant Repository

--- src/main/java/repository/ParticipantRepository.java
+++ src/main/java/repository/ParticipantRepository.java
@@ -11,12 +11,9 @@ import java.util.Optional;
 
 public interface ParticipantRepository extends Repository<Participant , Long> {
 
-    List<Participant> findAllParticipants();
     List<Participant> findByNameIgnoreCase(String name);
     List<Participant> findBySurnameIgnoreCase(String surname);
     Optional<Participant> findByEmailIgnoreCase(String email);
     List<Participant> findAllByRGPDStatus(RGPD_Status rgpdStatus);
     List<Participant> findAllBySession(@Param("sessionId") Long sessionId);
-    //List<Participant> findAllByGroupSession(@Param("sessionId") Long sessionId);
-    //List<Participant> findAllBySessionIgnoreCase(Session session);
 }
