05e2060b97d5f8becedc339a0df7021cb2736ce8
ToniR90 <romy90.t@gmail.com>
2025-08-21 03:01:23 +0200

    refactor: ParticipantRepository

--- src/main/java/repository/ParticipantRepository.java
+++ src/main/java/repository/ParticipantRepository.java
@@ -2,18 +2,17 @@ package repository;
 
 import entity.participant.abstractParticipant.Participant;
 import entity.participant.abstractParticipant.participantEnum.RGPD_Status;
-import entity.session.abstractSession.Session;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ParticipantRepository extends Repository<Participant , Long> {
+public interface ParticipantRepository extends JpaRepository<Participant , Long> {
 
     List<Participant> findByNameIgnoreCase(String name);
     List<Participant> findBySurnameIgnoreCase(String surname);
     Optional<Participant> findByEmailIgnoreCase(String email);
     List<Participant> findAllByRGPDStatus(RGPD_Status rgpdStatus);
-    List<Participant> findAllBySession(@Param("sessionId") Long sessionId);
+    List<Participant> findAllBySessionList_Id(@Param("sessionId") Long sessionId);
 }
