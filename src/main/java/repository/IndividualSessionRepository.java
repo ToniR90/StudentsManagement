e971c396f8ed0cfb535eafeccebb2bc05873015d
ToniR90 <romy90.t@gmail.com>
2025-08-21 03:24:04 +0200

    feat: IndividualSessionRepository

--- src/main/java/repository/IndividualSessionRepository.java
+++ src/main/java/repository/IndividualSessionRepository.java
@@ -1,7 +1,14 @@
 package repository;
 
 import entity.session.individualSession.IndividualSession;
+import entity.session.individualSession.individualSessionEnum.SessionAssistance;
+import entity.session.individualSession.individualSessionEnum.SessionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IndividualSessionRepository extends JpaRepository<IndividualSession , Long> {
+
+    List<IndividualSession> findBySessionAssistance(SessionAssistance assistance);
+    List<IndividualSession> findBySessionType(SessionType type);
 }
