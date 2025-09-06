7e971733062a199f5ed3e7213bb3245a8c97cf4d
ToniR90 <romy90.t@gmail.com>
2025-09-07 00:45:05 +0200

    feat: SessionController

--- backend/src/main/java/com/university/management/controller/SessionController.java
+++ backend/src/main/java/com/university/management/controller/SessionController.java
@@ -1,4 +1,35 @@
 package com.university.management.controller;
 
+import com.university.management.dto.session.SessionResponseDTO;
+import com.university.management.service.SessionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("sessions")
+@RequiredArgsConstructor
 public class SessionController {
+
+    private final SessionService sessionService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<SessionResponseDTO>> getAllSessions() {
+        List<SessionResponseDTO> sessions = sessionService.findAllSessions();
+        return ResponseEntity.ok(sessions);
+    }
+
+    @DeleteMapping("/by-id")
+    public ResponseEntity<SessionResponseDTO> deleteById(@RequestParam Long id) {
+        SessionResponseDTO session = sessionService.deleteSession(id);
+        return ResponseEntity.ok(session);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> countAll() {
+        Long count = sessionService.countAllSessions();
+        return ResponseEntity.ok(count);
+    }
 }
