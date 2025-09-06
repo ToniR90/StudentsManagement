dff2e16387ad62ac54e70d5b5d7c8180d2426cf3
ToniR90 <romy90.t@gmail.com>
2025-09-07 00:10:41 +0200

    feat: ExternalCollaboratorController

--- backend/src/main/java/com/university/management/controller/ExternalCollaboratorController.java
+++ backend/src/main/java/com/university/management/controller/ExternalCollaboratorController.java
@@ -1,9 +1,15 @@
 package com.university.management.controller;
 
+import com.university.management.dto.externalCollaborator.ExternalCollaboratorRequestDTO;
+import com.university.management.dto.externalCollaborator.ExternalCollaboratorResponseDTO;
 import com.university.management.service.ExternalCollaboratorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("external-collaborator")
@@ -11,4 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExternalCollaboratorController {
 
     private final ExternalCollaboratorService externalCollaboratorService;
+
+    @PostMapping
+    public ResponseEntity<ExternalCollaboratorResponseDTO> createExternalCollaborator(@Valid @RequestBody ExternalCollaboratorRequestDTO requestDTO) {
+        ExternalCollaboratorResponseDTO externalCollaboratorResponse = externalCollaboratorService.saveExternalCollaborator(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(externalCollaboratorResponse);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ExternalCollaboratorResponseDTO>> getAllExternalCollaborators() {
+        List<ExternalCollaboratorResponseDTO> responseList = externalCollaboratorService
+                .findAllExternalCollaborator();
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/by-affiliated-company")
+    public ResponseEntity<List<ExternalCollaboratorResponseDTO>> getByAffiliatedCompany(String affiliatedCompany) {
+        List<ExternalCollaboratorResponseDTO> responseList = externalCollaboratorService
+                .findByAffiliatedCompany(affiliatedCompany);
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> countAll() {
+        Long count = externalCollaboratorService.countAllExternalCollaborators();
+        return ResponseEntity.ok(count);
+    }
 }
