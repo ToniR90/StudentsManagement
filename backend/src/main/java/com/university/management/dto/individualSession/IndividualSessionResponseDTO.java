871a72e8d2d7cf66b1c0a26a9c9402e1d3d1e003
ToniR90 <romy90.t@gmail.com>
2025-09-07 14:55:33 +0200

    feat: IndividualSessionResponseDTO

--- backend/src/main/java/com/university/management/dto/individualSession/IndividualSessionResponseDTO.java
+++ backend/src/main/java/com/university/management/dto/individualSession/IndividualSessionResponseDTO.java
@@ -1,4 +1,23 @@
 package com.university.management.dto.individualSession;
 
-public class IndividualSessionResponseDTO {
+import com.university.management.dto.session.SessionResponseDTO;
+import com.university.management.entity.participant.abstractParticipant.Participant;
+import com.university.management.entity.participant.student.Student;
+import com.university.management.entity.session.individualSession.individualSessionEnum.SessionAssistance;
+import com.university.management.entity.session.individualSession.individualSessionEnum.SessionType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@SuperBuilder
+public class IndividualSessionResponseDTO extends SessionResponseDTO {
+    private Student student;
+    private String commitment;
+    private Set<Participant> relatedPeople;
+    private SessionAssistance assistance;
+    private SessionType sessionType;
 }
