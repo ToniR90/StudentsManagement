dee11ab6a111e8388861f3de138c3944485cb092
ToniR90 <romy90.t@gmail.com>
2025-09-09 13:18:02 +0200

    feat: IndividualSessionController

--- backend/src/main/java/com/university/management/controller/IndividualSessionController.java
+++ backend/src/main/java/com/university/management/controller/IndividualSessionController.java
@@ -1,4 +1,60 @@
 package com.university.management.controller;
 
+import com.university.management.dto.individualSession.IndividualSessionRequestDTO;
+import com.university.management.dto.individualSession.IndividualSessionResponseDTO;
+import com.university.management.entity.participant.student.Student;
+import com.university.management.entity.session.individualSession.IndividualSession;
+import com.university.management.entity.session.individualSession.individualSessionEnum.SessionAssistance;
+import com.university.management.entity.session.individualSession.individualSessionEnum.SessionType;
+import com.university.management.service.IndividualSessionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("individual-session")
+@RequiredArgsConstructor
 public class IndividualSessionController {
+
+    private final IndividualSessionService individualSessionService;
+
+    @PostMapping
+    public ResponseEntity<IndividualSessionResponseDTO> createIndividualSession(@Valid @RequestParam IndividualSessionRequestDTO individualSessionRequest) {
+        IndividualSessionResponseDTO individualSessionResponse = individualSessionService.saveIndividualSession(individualSessionRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(individualSessionResponse);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<IndividualSessionResponseDTO>> getAllIndividualSessions() {
+        List<IndividualSessionResponseDTO> responseList = individualSessionService.findAllIndividualSessions();
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/by-student")
+    public ResponseEntity<List<IndividualSessionResponseDTO>> getByStudent(Student student) {
+        List<IndividualSessionResponseDTO> responseList = individualSessionService.findByStudent(student);
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/by-assistance")
+    public ResponseEntity<List<IndividualSessionResponseDTO>> getByAssistance(SessionAssistance assistance) {
+        List<IndividualSessionResponseDTO> responseList = individualSessionService.findBySessionAssistance(assistance);
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/by-session-type")
+    public ResponseEntity<List<IndividualSessionResponseDTO>> getBySessionType(SessionType sessionType) {
+        List<IndividualSessionResponseDTO> responseList = individualSessionService.findBySessionType(sessionType);
+        return ResponseEntity.ok(responseList);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> countAllIndividualSessions() {
+        Long count = individualSessionService.countAllIndividualSessions();
+        return ResponseEntity.ok(count);
+    }
 }
